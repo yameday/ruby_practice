77c01272751f7444fca47751cdd01c8e93f47509
yameday <a1989198@hotmail.com>
2017-09-23 00:11:55 +0800

    revise comment

--- n2.rb
+++ n2.rb
@@ -2,25 +2,27 @@
 
 
 def n2(n)
+  #處理例外
   if n==1
     return true
   elsif n <= 0 || n%2!=0
     return false
   else
-
+    #如果餘數為零繼續除
     while (n % 2==0)
       n=n/2
-    end  
-      if n==1
+    end
+    #如果n=1就是 2的次方
+    if n==1
         return true
-      else
+    else
         return false 
-      end
+    end
   end
 end
 
 
-puts n2(0)
-puts n2(-15)
-puts n2(1)
-puts n2(32)
+puts n2(0)    #false
+puts n2(-15)  #false
+puts n2(1)    #true
+puts n2(32)   #true
